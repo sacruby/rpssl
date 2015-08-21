0719bc6684d1b59bc022b4d4e3119ce312ae310f
Christopher Haupt <chaupt@mobirobo.com>
2015-08-20 21:01:29 -0700

    Our random player

---
+++ players/cfh_random_player.rb
@@ -0,0 +1,13 @@
+require_relative '../rpssl'
+
+class CfhRandomPlayer < Player
+  MOVES = [:spock, :rock, :paper, :scissors, :lizard, :spock]
+
+  def initialize(name)
+    super
+  end
+
+  def choose
+    MOVES.sample
+  end
+end
